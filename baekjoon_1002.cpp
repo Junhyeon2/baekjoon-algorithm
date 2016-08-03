f4b9e3eb99dcc58dd7414674c445cfd2a3f96f70
Jun Hyeon KIM <junhyeon2@naver.com>
2016-08-03 09:15:48 +0900

    Q - 1002 complete!

--- baekjoon_1002.cpp
+++ baekjoon_1002.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <algorithm>
 #include <cmath>
 using namespace std;
-
+//r'이 r보다 크거나 같다 가정.
+/* 두점에서 만날 때
+ * r'-r<d<r'+r
+ */
+/* 한점에서 만날 때
+ * 외접: r+r' = d
+ * 내접: r'-r = d
+ */
+ /* 만나지 않을 때
+  * 외부: r+r' < d
+  * 내부: d < r'-r
+  * 동심원: d = 0
+  */
 int main(void){
     int tc;
 
@@ -12,26 +25,22 @@ int main(void){
         cin>>x1>>y1>>r1;
         cin>>x2>>y2>>r2;
 
-        double dist = sqrt(pow(x2- x1, 2) + pow(y2 - y1, 2));
+        double d = sqrt(pow(x2- x1, 2) + pow(y2 - y1, 2));
+        int big = max(r1, r2);
+        int small = min(r1, r2);
 
-        if(dist == 0.0){
-            if(r1 == r2)
+        if(d == 0.0){
+            if(big == small)
                 ret = -1;
             else
                 ret = 0;
         }else{
-           if(r1+r2 < dist)
-                ret = 0;
-            else if(r1+r2 == dist || abs(r1-r2) == dist)
+            if(big-small < d && big+small > d)
+                ret = 2;
+            else if(big+small == d || big-small == d)
                 ret = 1;
-            else{
-                if(r1 > r2){
-                    if(r1 > 2*r2)
-                        ret = 0;
-                }else
-                    if(2*r1 < r2)
-                        ret = 0;
-            }
+            else
+                ret = 0;
         }
         cout<<ret<<endl;
     }
